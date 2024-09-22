5f814e779c6ec4fdbd5a736c8307186e26a39871
Cyclingbob <bob21567@yahoo.com>
2024-09-22 10:38:30 +0100

    Upgrade to Python 3

--- commander.py
+++ commander.py
@@ -1,81 +1,76 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 import time
 import os
 import csv
 
-#Add current directory to system path
+# Add current directory to system path
 sys.path.append(os.getcwd())
 
-#import the maplinrobot class
+# Import the maplinrobot class
 from maplinrobot import MaplinRobot
 
 class RobotCommander():
-	def __init__(self):
-		self.csvfile=''
-		self.commands=''
+    def __init__(self):
+        self.csvfile = ''
+        self.commands = ''
 
+    def ParseArguments(self):
+        try:
+            if len(sys.argv) > 1:  # Valid number of arguments
+                self.csvfile = str(sys.argv[1])
+                return True
+            else:
+                return False
+        except IndexError:
+            print("None or wrong number of arguments supplied!\n")
+            return False
 
-	def ParseArguments(self):
-		try:
-			if len(sys.argv) > 1: # Valid number of arguments
-				self.csvfile=str(sys.argv[1])
-				return True
-			else:
-				return False
-		except IndexError:
-			print "None or wrong number of arguments supplied!\n"
-			return False
+    def CheckFileExists(self):
+        if os.path.isfile(self.csvfile) and os.path.exists(self.csvfile):
+            return True
+        else:
+            return False
 
-	def CheckFileExists(self):
-		if os.path.isfile(self.csvfile) and os.path.exists(self.csvfile):
-			return True
-		else:
-			return False
+    def ReadCSVFile(self):
+        try:
+            if self.ParseArguments() and self.CheckFileExists():
+                with open(self.csvfile, newline='') as csvfile:
+                    reader = csv.reader(csvfile, delimiter=',')
+                    self.commands = list(reader)
+                return True
+            else:
+                return False
+        except csv.Error:
+            print("Unable to load csv file\n")
+            return False
 
-	def ReadCSVFile(self):
-		try:
-			if self.ParseArguments() and self.CheckFileExists():
-				self.commands = csv.reader(open(self.csvfile), delimiter=',')
-				return True
-			else:
-				return False
-		except csv.Error:
-			print "Unable to load csv file\n"
-			return False
+    def RunCommands(self):
+        try:
+            r = MaplinRobot()
+            if self.ReadCSVFile():
+                for row in self.commands:
+                    print(f"Running command '{str(row[0])}' for a duration of {float(row[1])} second(s) with a pause of {float(row[2])} second(s)")
+                    if r.MoveArm(t=float(row[1]), cmd=str(row[0])):
+                        time.sleep(float(row[2]))
+                    else:
+                        print("Problem sending commands. Exiting\n")
+                        return False
 
-	def RunCommands(self):
-		try:
-			r = MaplinRobot()
-			if self.ReadCSVFile():
-				for row in self.commands:
-					print "Running command '%s' for a duration of %f second(s) with a pause of %f second(s)" %(str(row[0]), float(row[1]), float(row[2]))
-					if r.MoveArm(t=float(row[1]), cmd=str(row[0])):
-						time.sleep(float(row[2]))
-					else:
-						print "Problem sending commands. Exiting\n"
-						return False
+                # All commands done. Stop the arm
+                print("\nAll commands executed. Stopping the arm\n")
+                r.StopArm()
+                return True
+            else:
+                print("Something went wrong!\n")
+                r.StopArm()
+                return False
 
-				#All commands done. Stop the arm
-				print "\nAll commands executed. Stopping the arm\n"
-				r.StopArm()
-				return True
-			else:
-				print "Something went wrong!\n"
-				r.StopArm()
-				return False
+        except KeyboardInterrupt:
+            print("Ctrl-C pressed. Exiting\n")
+            return False
 
-		except KeyboardInterrupt:
-			print "ctrl-c pressed. Exiting\n"
-			#r.StopArm()
-			return False
-
-
+# Instantiate and run the robot commander
 r = RobotCommander()
 r.ParseArguments()
 r.RunCommands()
-
-
-
-
-
